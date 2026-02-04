3c5ce31ae1fdcc24e1476d35e60f437ef917dd6a
Kaylyn <kngroom@uab.edu>
2026-02-03 19:06:23 -0600

    Initial Commit

---
+++ MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by Kaylyn Groom on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
